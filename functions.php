f34e451ba99df57a1537e9da4a0ae576a4dcdead
azizultex <azizultex@gmail.com>
2016-09-02 18:35:33 +0600

    custom title

--- functions.php
+++ functions.php
@@ -11,3 +11,28 @@ function my_theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+function oo_custom_title( $title, $sep ) {
+	global $paged, $page;
+ 
+	if ( is_feed() ) {
+		return $title;
+	} // end if
+ 
+	// Add the site name.
+	$title .= get_bloginfo( 'name' );
+ 
+	// Add the site description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) ) {
+		$title = "$title $sep $site_description";
+	} // end if
+ 
+	// Add a page number if necessary.
+	if ( $paged >= 2 || $page >= 2 ) {
+		$title = sprintf( __( 'Page %s', 'mayer' ), max( $paged, $page ) ) . " $sep $title";
+	} // end if
+	return $title;
+}
+add_filter( 'wp_title', 'oo_custom_title', 10, 2 );
