0f23aa210ed0d91c209d87ee9c9c5060f3cd294e
sumanengbd <sumanengbd@gmail.com>
2024-11-29 01:02:00 +0600

    My Post View Function Added

--- functions.php
+++ functions.php
@@ -86,6 +86,28 @@ add_filter('get_the_archive_title', function ($title) {
     return $title;
 });
 
+/*** Popular Posts */
+function azizultex_count_post_visits() {
+    
+    if ( is_single() ) 
+    {
+        global $post;
+
+        $views = get_post_meta( $post->ID, 'my_post_viewed', true );
+
+        if ( $views == '' ) 
+        {
+            update_post_meta( $post->ID, 'my_post_viewed', '1' );   
+        } 
+        else 
+        {
+            $views_no = intval( $views );
+            
+            update_post_meta( $post->ID, 'my_post_viewed', ++$views_no );
+        }
+    }
+}
+add_action( 'wp_head', 'azizultex_count_post_visits' );
 
 /*** Added Page Custom Meta */
 function azizultex_add_page_meta_box() {
